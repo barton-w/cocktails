cc4700d6b7c2cf6bd771a5b52b3bfc96cbfcd733
barton-w <bartonw@gmail.com>
2019-04-27 17:48:01 -0600

    Spirit-search functionality is hitting the API correctly

--- app/app.js
+++ app/app.js
@@ -2,12 +2,11 @@
 const baseURL = "https://www.thecocktaildb.com/api/json/v1/";
 const guid = "1";  //this can be replaced by an API key should I decide to use premium features of the API
 const randomEndpoint = "/random.php";
-const ingredientEndpoint = "/search.php?i=";
+const ingredientEndpoint = "/filter.php?i=";
 
 //function to return an array of alcoholic cocktails from the API
 const getRandCocktails = (num) => {
-  const randCocktails = [];
-  for (let i = 1; i <= num; i++) {
+  for (let i = 0; i < num; i++) {
     $.ajax({
       url: baseURL+guid+randomEndpoint
     }).then((cocktail) => {
@@ -27,7 +26,31 @@ const displayCocktail = (obj) => {
   $("#random").append($cocktail);
 }
 
+//function to receive user input from the form, and hit the API's ingredient endpoint
+const getSearchCocktails = (value, num) => {
+  $.ajax({
+    url: baseURL+guid+ingredientEndpoint+value
+  }).then((obj) => {
+    const cocktailIdArray = [];
+    //push 4 random cocktails to an array
+    for (let i = 0; i < num; i++) {
+      let position = Math.floor(Math.random()*obj.drinks.length);
+      cocktailIdArray.push(obj.drinks[position].idDrink);
+    };
+    getSearchCocktailDetails(cocktailArray);
+  }, (error) => {
+    console.log(error);
+  });
+};
+
 //jQuery document ready function
 $(() => {
-  getRandCocktails(4);
+  //on page load, populate the random section with 4 cocktails
+  // getRandCocktails(4);
+
+  //event listener for the form
+  $("#spirit-form").submit((event) => {
+    getSearchCocktails($("#spirit").val(), 4);
+    event.preventDefault();
+  });
 });
