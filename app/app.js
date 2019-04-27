325d57ec23f27234b574d4bb3aa79613e546b1f1
barton-w <bartonw@gmail.com>
2019-04-27 17:16:32 -0600

    Random cocktail query is working, and data is displayed on the page

--- app/app.js
+++ app/app.js
@@ -1,31 +1,33 @@
-//establish variables to be used in requests to the API
+//establishing global variables to be used in requests to the API
 const baseURL = "https://www.thecocktaildb.com/api/json/v1/";
 const guid = "1";  //this can be replaced by an API key should I decide to use premium features of the API
 const randomEndpoint = "/random.php";
 const ingredientEndpoint = "/search.php?i=";
-//function to get random alcoholic cocktails and push them into an array
-const randCocktail = (num) => {
-  let randCocktailArray = [];
+
+//function to return an array of alcoholic cocktails from the API
+const getRandCocktails = (num) => {
+  const randCocktails = [];
   for (let i = 1; i <= num; i++) {
     $.ajax({
       url: baseURL+guid+randomEndpoint
     }).then((cocktail) => {
-      randCocktailArray.push(cocktail);
+      displayCocktail(cocktail.drinks[0]);
     }, (error) => {
       console.log(error);
-      return;
     });
   };
-  console.log(randCocktailArray);
 };
+
+//function that gets called by getRandCocktails to display them on the page
+const displayCocktail = (obj) => {
+  console.log(obj);
+  const $cocktail = $("<div>").addClass("cocktail").text(obj.strDrink);
+  const $image = $("<img>").attr("src", obj.strDrinkThumb);
+  $cocktail.append($image);
+  $("#random").append($cocktail);
+}
 
 //jQuery document ready function
 $(() => {
-  // $.ajax({
-  //   url: "https://www.thecocktaildb.com/api/json/v1/1/random.php?",
-  //   success: (result) => {
-  //     console.log(result);
-  //   }
-  // });
-  randCocktail(3);
+  getRandCocktails(4);
 });
